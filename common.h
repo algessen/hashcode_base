c37d20d01ddc6e9c18b9b3a1db3a04028369fb4d
Alexandra Gessen <algessen@yandex-team.ru>
2021-02-17 18:21:34 +0300

    Initial commit

---
+++ common.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <bitset>
+#include <random>
+
+using namespace std;
